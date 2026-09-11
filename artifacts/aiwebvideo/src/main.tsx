6ccc8cb0a1eab2a113acd9678d838d7f33ebeddb
mohammadadeela <120398888+mohammadadeela@users.noreply.github.com>
2026-09-11 19:44:05 +0300

    Remove legacy finished chat layout overrides

--- artifacts/aiwebvideo/src/main.tsx
+++ artifacts/aiwebvideo/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 import './index.css';
-import './finished-chat.css';
 import './finished-chat-compact.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
