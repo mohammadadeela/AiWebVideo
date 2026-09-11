1e09ed835bf109dfacec33617bf041edc6682f14
mohammadadeela <120398888+mohammadadeela@users.noreply.github.com>
2026-09-11 18:25:33 +0300

    Load compact finished chat styles

--- artifacts/aiwebvideo/src/main.tsx
+++ artifacts/aiwebvideo/src/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 import './index.css';
+import './finished-chat.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
