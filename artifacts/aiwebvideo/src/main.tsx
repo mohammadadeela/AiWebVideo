48d2ed67e0419a432e84fb8a54c6cdf503cab920
mohammadadeela <120398888+mohammadadeela@users.noreply.github.com>
2026-09-11 18:50:33 +0300

    Load final compact finished chat overrides

--- artifacts/aiwebvideo/src/main.tsx
+++ artifacts/aiwebvideo/src/main.tsx
@@ -4,5 +4,6 @@ import App from './App';
 
 import './index.css';
 import './finished-chat.css';
+import './finished-chat-compact.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
