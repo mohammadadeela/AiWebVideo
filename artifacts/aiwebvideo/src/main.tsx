ca3e02e3b5ae122cf859fd0a7c76586a06c52427
mohammadadeela <120398888+mohammadadeela@users.noreply.github.com>
2026-09-16 18:06:44 +0300

    Load restored creator CTA layout

--- artifacts/aiwebvideo/src/main.tsx
+++ artifacts/aiwebvideo/src/main.tsx
@@ -4,5 +4,6 @@ import App from './App';
 
 import './index.css';
 import './finished-chat-compact.css';
+import './creator-cta-position.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
